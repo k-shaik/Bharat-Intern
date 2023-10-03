55103328f9697b01f7b5c0faac1cc08c37033833
Khaja Shaik <92737226+k-shaik@users.noreply.github.com>
2023-10-03 23:31:34 +0530

    Inital Commit

---
+++ Proflito-Page/js/commands.js
@@ -0,0 +1,80 @@
+var password = "123456";
+var email = 'mailto:khajashaik070404@gmail.com';
+
+var whoami = [
+  "<br>",
+  "Hey, I'm Khaja_Shaik👋",
+  "Cyber Security Enthusiast || UG '25 major in Computer Science and Information Technology at KL University",
+  "The beauty of cybersecurity lies in its constant challenge to outwit the unseen enemy.",
+  "Head & Mentor in Cyber Security Club (Student Activity Center)",
+  "<br>"
+];
+
+var objective = [
+  "<br>",
+  "Who am I? I'm like a pixel in the digital universe, a byte-sized comedian ready to tickle your funny bone..",
+  "<br>"
+];
+
+var social = [
+  "<br>",
+  'linkedin       <a href="https://www.linkedin.com/in/khaja-shaik-23690b226/" target="_blank">Linkedin/khaja_Shaik</a>',
+  'instagram      <a href="https://www.instagram.com/who_is_khaja_shaik/" target="_blank">Instagram.com/khaja_Shaik</a>',
+  'github         <a href="https://github.com/khajashaik2004" target="_blank">github.com/khaja_Shaik</a>',
+  
+  "<br>"
+];
+
+var secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+var projects = [
+  "<br>",
+  'Python full Stack devolpment         <a href="http://cryptorush.pythonanywhere.com/" target="_blank">Crypto-rush</a>',
+  "<br>"
+];
+
+
+var Certifications = [
+  "<br>",
+  'Azure Fundamentals(AZ-900)        <a href="https://www.credly.com/badges/3187d6c7-7560-4722-8b8f-61f136cd9bf7/public_url" target="_blank">Verify_here</a>',
+  'Cybersecurity Essentials          <a href="https://www.credly.com/badges/7660f997-8e44-4c3a-a159-eb76747af190/public_url" target="_blank">Verify_here</a>', 
+  'Networking Essentials             <a href="https://www.credly.com/badges/9a7d08d5-c797-46ca-86de-ba72817fd66b/public_url" target="_blank">Verify_here</a>', 
+  "<br>"
+];
+
+
+var help = [
+  "<br>",
+  '<span class="command">whoami</span>          Who is Khaja_shaik?',
+  '<span class="command">Objective</span>       Who are you?',
+  '<span class="command">social</span>          Display social networks',
+  '<span class="command">secret</span>          Find the password',
+  '<span class="command">certifications</span>  Display the badges',
+  '<span class="command">projects</span>        View coding projects',
+  '<span class="command">history</span>         View command history',
+  '<span class="command">help</span>            You obviously already know what this does',
+  '<span class="command">email</span>           You can email me',
+  '<span class="command">clear</span>           Clear terminal',
+  '<span class="command">banner</span>          Display the header',
+  "<br>",
+];
+
+
+var banner = [
+  '<span class="index">Welcome to MyTerminal v1.0.</span>',
+
+
+ " ██████╗░░█████╗░██████╗░████████╗███████╗░█████╗░██╗░░░░░██╗░█████╗░  ██████╗░░█████╗░░██████╗░███████╗",
+ " ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██║░░░░░██║██╔══██╗  ██╔══██╗██╔══██╗██╔════╝░██╔════╝",
+  "██████╔╝██║░░██║██████╔╝░░░██║░░░█████╗░░██║░░██║██║░░░░░██║██║░░██║  ██████╔╝███████║██║░░██╗░█████╗░░",
+ " ██╔═══╝░██║░░██║██╔══██╗░░░██║░░░██╔══╝░░██║░░██║██║░░░░░██║██║░░██║  ██╔═══╝░██╔══██║██║░░╚██╗██╔══╝░░",
+ " ██║░░░░░╚█████╔╝██║░░██║░░░██║░░░██║░░░░░╚█████╔╝███████╗██║╚█████╔╝  ██║░░░░░██║░░██║╚██████╔╝███████╗",
+  "╚═╝░░░░░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░░╚════╝░╚══════╝╚═╝░╚════╝░  ╚═╝░░░░░╚═╝░░╚═╝░╚═════╝░╚══════╝",
+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="color2">Welcome to my portfolio web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
